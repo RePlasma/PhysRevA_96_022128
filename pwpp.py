69f481e9a1572edd79fd21001aa7237ad283ac7b
Oscar Amaro <oscar.amaro@tecnico.ulisboa.pt>
2023-02-08 10:03:29 +0000

    better plots and vectorization

--- pwpp.py
+++ pwpp.py
@@ -5,9 +5,13 @@ Plane Wave Pair Production - pwpp scripts
 References:
 # [1] https://journals.aps.org/pra/abstract/10.1103/PhysRevA.96.022128
 """
+from tqdm import tqdm, trange
+import json
+import scipy.integrate as integrate
+import scipy.special as special
 # numpy
 import numpy as np
-np.random.seed(19680801)
+np.random.seed(123)
 from numpy.random import default_rng
 rng = default_rng()
 # import functions
@@ -27,58 +31,75 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
-class MathTextSciFormatter(mticker.Formatter):
-    def __init__(self, fmt="%1.2e"):
-        self.fmt = fmt
-    def __call__(self, x, pos=None):
-        s = self.fmt % x
-        decimal_point = '.'
-        positive_sign = '+'
-        tup = s.split('e')
-        significand = tup[0].rstrip(decimal_point)
-        sign = tup[1][0].replace(positive_sign, '')
-        exponent = tup[1][1:].lstrip('0')
-        if exponent:
-            exponent = '10^{%s%s}' % (sign, exponent)
-        if significand and exponent:
-            s =  r'%s{\times}%s' % (significand, exponent)
-        else:
-            s =  r'%s%s' % (significand, exponent)
-        return "${}$".format(s)
+def g(x):
+    """
+        g function
+    """
+    return ( 1 + 4.8*(1+x)*log(1+1.7*x) + 2.44*x**2 )**(-2/3)
 
-def TErber(chi):
-    return 0.16/chi * kv(1/3,4/(3*chi))**2
+def ppm(a0,w0,n,w):
+    """
+        eq (4) pair creation
+    """
+    return alpha * a0 * n * scR((2*a0*w0*w)/(m**2))
 
-# auxiliary functions
+def scR(x):
+    """
+        eq (5) auxiliary functional of P±
+    """
+    return (0.453*kv(1/3,(4)/(3*x))**2) / (1+0.145*x**(1/4)*log(1+2.26*x)+0.330*x)
+
+def phic(g0, a0, w0, n):
+    """
+        eq (7) critical phase (phic is implicit function of chic)
+    """
+    return sqrt( (2*pi**2*n**2/log(2))*log((2*g0*a0*w0)/(chic(g0,a0,w0,n)*m)) )
+
 def chic(g0, a0, w0, n):
     """
-    [1] critical chi
+        eq (8) critical chi (implicit)
     """
-    #if a0 > 0.1:
     def nonlin(chi):
-        return chi**4 * g(chi)**2 - (72*log(2))/(pi**2*alpha**2)*((g0*w0)/(n*m))**2*log((2*g0*a0*w0)/(m*chi))
+        return chi**4 * g(chi)**2 - (72*log(2))/(pi**2*alpha**2)*((g0*w0)/(n*m))**2 * log((2*g0*a0*w0)/(m*chi))
     res = fsolve(nonlin, 1e-10)
-    #else:
-    #    res = 1e-5
     return res
+
+def Omega(g0,a0,w0,n):
+    """
+        eq (13) radiated energy
+    """
+    return (sqrt(2*pi)*g0*m) * ( (2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))))/( 1+2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))) ) )**(1/2)
 
 def chicrr(g0,a0,w0,n):
     """
-    [1] eq (14) critical chi with radiation reaction
+        eq (14) critical chi with radiation reaction
     """
-    res = chic(g0,a0,w0,n)*(1+Omega(g0,a0,w0,n)/(2*g0*m))**(-1);
+    res = chic(g0,a0,w0,n) / (1+Omega(g0,a0,w0,n)/(2*g0*m)) ;
     return res
+
+def gphi():
+    """
+        eq (16) electron energy assuming "radiated power and χ as functions of phase are approximately Gaussian in form"
+    """
+    sigma = sigsq(g0,a0,w0,n)
+    return gf + ( g0*Omega/(2*g0*m+Omega) ) * (1+erf((phi-phic)/(sqrt(2)*sigma)))
 
 def chiphi(g0,a0,w0,n,phi):
     """
-    [1] eq (17) chi in gaussian form
+        eq (17) chi in gaussian form
     """
     res = (chic(g0,a0,w0,n))/(1+(Omega(g0,a0,w0,n))/(2*g0*m)) * exp(-(phi-phic(g0,a0,w0,n))**2/(2*sigsq(g0,a0,w0,n)));
     return res
 
+def sigsq(g0,a0,w0,n):
+    """
+        eq (18) sigma squared
+    """
+    return (pi**2*n**2)/(log(2)) * (1+2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))))**(-1)
+
 def dNgdw(g0,a0,w0,n,w):
     """
-    [1] eq (20) photon spectra
+        eq (20) photon spectra
     """
     chi0 = 2*g0*a0*w0/m;
     res = (sqrt(3)*pi*alpha*Fhe(g0,a0,w0,n))/(sqrt(2*log(2))) * (a0*n)/(sqrt(g0*m)) * (chicrr(g0,a0,w0,n)/chi0)/(sqrt(1+2*log(chi0/chic(g0,a0,w0,n)))) * (exp(-(2*w)/(3*chicrr(g0,a0,w0,n)*(g0*m-w))))/(sqrt(3*chicrr(g0,a0,w0,n)*(g0*m-w)+4*w));
@@ -86,83 +107,46 @@ def dNgdw(g0,a0,w0,n,w):
 
 def Fhe(g0,a0,w0,n):
     """
-    [1] eq (21) hard photon correction
+        eq (21) hard photon correction
     """
     arg = np.real((sqrt(2*log(2))*phic(g0,a0,w0,n)/(2*pi*n)));
     res = 0.5*(1-erf(arg));
     return res
 
-def g(x):
+def wc(g0, a0, w0, n):
     """
-    [1] g function
+        eq (23) critical frequency
     """
-    return (1+4.8*(1+x)*log(1+1.7*x)+2.44*x**2)**(-2/3)
-
-def gp(g0, a0, w0, n):
-    """
-    [1] eq (25) positron average energy
-    """
-    wcc = wc(g0,a0,w0,n);
-    res = (wcc/(2*m))*(1+(pi**(3/2)*alpha/(3*sqrt(2*log(2))))*(n*a0**2*w0*wcc/m**2)*g(a0*w0*wcc/m**2))**(-1);
-    return res
+    return (g0*m) * (sqrt( (2*chicrr(g0,a0,w0,n)*m)/(a0*g0*w0) ))/(1+sqrt( (2*chicrr(g0,a0,w0,n)*m)/(a0*g0*w0) ))
 
 def Np(g0, a0, w0, n):
     """
-    [1] eq (24) positron yield
+        eq (24) positron yield
     """
     wcc = wc(g0,a0,w0,n);
     res = (3*sqrt(pi)*ppm(a0,w0,n,wcc)*chicrr(g0,a0,w0,n)/sqrt(2)) * ((g0*m-wcc)**2/(g0*m)) * dNgdw(g0,a0,w0,n,wcc);
     return res
 
-def Omega(g0,a0,w0,n):
+def gp(g0, a0, w0, n):
     """
-    [1] eq (13) radiated energy
+        eq (25) positron average energy
     """
-    return (sqrt(2*pi)*g0*m) * ( (2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))))/( 1+2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))) ) )**(1/2)
+    wcc = wc(g0,a0,w0,n);
+    res = (wcc/(2*m))*(1+(pi**(3/2)*alpha/(3*sqrt(2*log(2))))*(n*a0**2*w0*wcc/m**2)*g(a0*w0*wcc/m**2))**(-1);
+    return res
 
-def phic(g0, a0, w0, n):
+def TErber(chi):
     """
-    [1] critical phase
+        eq (A4) Erber approximation of pair creation rate
     """
-    return sqrt( (2*pi**2*n**2/log(2))*log((2*g0*a0*w0)/(chic(g0,a0,w0,n)*m)) )
-
-def ppm(a0,w0,n,w):
-    """
-    [1] eq (4) pair creation
-    """
-    return alpha * a0 * n * scR((2*a0*w0*w)/(m**2))
-
-def scR(x):
-    """
-    [1]  critical phase
-    """
-    return (0.453*kv(1/3,(4)/(3*x))**2) / (1+0.145*x**(1/4)*log(1+2.26*x)+0.330*x)
-
-def sigsq(g0,a0,w0,n):
-    """
-    [1] eq (18) sigma squared
-    """
-    return (pi**2*n**2)/(log(2)) * (1+2*log((2*g0*a0*w0)/(m*chic(g0,a0,w0,n))))**(-1)
-
-def wc(g0, a0, w0, n):
-    """
-    [1] eq (23) critical frequency
-    """
-    return (g0*m) * (sqrt( (2*chicrr(g0,a0,w0,n)*m)/(a0*g0*w0) ))/(1+sqrt( (2*chicrr(g0,a0,w0,n)*m)/(a0*g0*w0) ))
-
-def gauss3D(z,x,y,a0,W0,lbd):
-    """
-    laser field 3D
-    """
-    zR = pi*W0**2/lbd
-    return a0/sqrt(1+(z/zR)**2) * exp(-(x**2+y**2)/(W0**2*(1+(z/zR)**2)))
+    return 0.16/chi * kv(1/3,4/(3*chi))**2
 
 """
 Specific to reproduce plots in [1]
 """
 def chicmod(g0, a0, w0, n):
     """
-    [1] critical chi mod (fig 3)
+        critical chi mod (fig 3)
     """
     def nonlin(chi):
         return chi**4 - (72*log(2))/(pi**2*alpha**2)*((g0*w0)/(n*m))**2*log((2*g0*a0*w0)/(m*chi));
@@ -170,13 +154,13 @@ def chicmod(g0, a0, w0, n):
 
 def chiphimod(g0,a0,w0,n,phi):
     """
-    [1] eq  (17) chi in gaussian form mod, assume Omega~0
+        eq (17) chi in gaussian form mod, assume Omega~0
     """
     return (chic(g0,a0,w0,n)) * exp(-(phi)**2/(2*sigsq(g0,a0,w0,n)));
 
 def chiphimod2(g0,a0,w0,n,phi):
     """
-    [1]  eq (16) and eq (6)
+        eq (16) and eq (6)
     """
     sigsqq = sigsq(g0,a0,w0,n);
     Omegaa = Omega(g0,a0,w0,n);
@@ -187,32 +171,39 @@ def chiphimod2(g0,a0,w0,n,phi):
 
 def dNgdwmod(g0,a0,w0,n,w):
     """
-    [1]  eq  (20) photon spectra modified, assume chicrr->chic
+        eq (20) photon spectra modified, assume chicrr->chic
     """
     chi0 = 2*g0*a0*w0/m;
     return (sqrt(3)*pi*alpha*Fhe(g0,a0,w0,n))/(sqrt(2*log(2))) * (a0*n)/(sqrt(g0*m)) * (chicrr(g0,a0,w0,n)/chi0)/(sqrt(1+2*log(chi0/chic(g0,a0,w0,n)))) * (exp(-(2*w)/(3*chic(g0,a0,w0,n)*(g0*m-w))))/(sqrt(3*chic(g0,a0,w0,n)*(g0*m-w)+4*w));
 
 def Npmod(g0, a0, w0, n):
     """
-    [1] eq (24) positron yield without radiation reaction
+        eq (24) positron yield without radiation reaction
     """
     wcc = wcmod(g0,a0,w0,n);
     return (3*sqrt(pi)*ppm(a0,w0,n,wcc)*chic(g0,a0,w0,n)/sqrt(2)) * ((g0*m-wcc)**2/(g0*m)) * dNgdw(g0,a0,w0,n,wcc);
 
 def phicmod(g0, a0, w0, n):
     """
-    [1]  critical phase mod (fig 3)
+        critical phase mod (fig 3)
     """
     return sqrt( (2*pi**2*n**2/log(2))*log((2*g0*a0*w0)/(chicmod(g0,a0,w0,n)*m)) );
 
 def wcmod(g0, a0, w0, n):
     """
-    [1] eq (23) critical frequenc, no rrr
+        eq (23) critical frequenc, no rrr
     """
     return (g0*m) * (sqrt( (2*chic(g0,a0,w0,n)*m)/(a0*g0*w0) ))/(1+sqrt( (2*chic(g0,a0,w0,n)*m)/(a0*g0*w0) ));
 
 def arraycenter(x):
     """
-    returns centered array
+        returns centered array
     """
-    return [(x[i]+x[i+1])/2 for i in range(len(x)-1)]
+    return np.array([(x[i]+x[i+1])/2 for i in range(len(x)-1)])
+
+def gauss3D(z,x,y,a0,W0,lbd):
+    """
+        Gaussian laser vector potential 3D
+    """
+    zR = pi*W0**2/lbd
+    return a0/sqrt(1+(z/zR)**2) * exp(-(x**2+y**2)/(W0**2*(1+(z/zR)**2)))
